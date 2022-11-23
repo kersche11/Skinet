4ddbd4f058390d464bea7287babc1a1045455db4
Kerschbaumer Gerald (IFKL IT OS ESS SD 1) <Gerald.Kerschbaumer@infineon.com>
2022-11-23 11:04:35 +0100

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,8 +11,30 @@ builder.Services.AddDbContext<StoreContext>(x=>x.UseSqlite(connectionString));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Repositorys
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
 
 var app = builder.Build();
+
+//Migrations and DB Update
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>(); 
+    try
+    {
+        var context=services.GetRequiredService<StoreContext>();
+        await context.Database.MigrateAsync();
+        //Dataseeding (Data/StoreContextSeed)
+        await StoreContextSeed.SeedAsync(context, loggerFactory);
+    }   
+    catch(Exception ex)
+    {
+        var logger=loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex,"An error occured during migration");
+    }
+    
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
