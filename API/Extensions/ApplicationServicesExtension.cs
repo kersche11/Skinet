66965ee374e50341a7088eae7060b542573cf5c7
gerKerschb <gerald.kerschbaumer@infineon.com>
2023-11-04 13:27:04 +0100

    Install Redis and Docker

--- API/Extensions/ApplicationServicesExtension.cs
+++ API/Extensions/ApplicationServicesExtension.cs
@@ -3,40 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Errors;
+using API.Helpers;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace API.Extensions
 {
     public static class ApplicationServicesExtension
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services )
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
-            
-            //Repositorys
-            services.AddScoped<IProductRepository,ProductRepository>();
-            //IGenericRepo
-            services.AddScoped(typeof(IGenericRepository<>),(typeof(GenericRepository<>)));
-            //Errorhandling
-            services.Configure<ApiBehaviorOptions>(options=>
-            {
-                 options.InvalidModelStateResponseFactory = actionContext => 
-                {
-                    var errors = actionContext.ModelState
-                        .Where(e=>e.Value.Errors.Count>0)
-                        .SelectMany(x=>x.Value.Errors)
-                        .Select(x=>x.ErrorMessage).ToArray();            
-    
-                    var errorResponse = new ApiValidationErrorResponse
-                        {
-                            Errors=errors
-                        };
+            //AutoMapper
+            services.AddAutoMapper(typeof(MappingProfiles));
+            //Database connection
+            services.AddDbContext<StoreContext>(x => x.UseSqlite(config.GetConnectionString("DefaultConnection")));
 
-                    return new BadRequestObjectResult(errorResponse);
-                };
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(options);
             });
+             //CORS
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            });
+            services.AddScoped<IBasketRepository,BasketRepository>();
+             //Repositorys
+            services.AddScoped<IProductRepository, ProductRepository>();
+            //IGenericRepo
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+             //Errorhandling
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+               {
+                   var errors = actionContext.ModelState
+                       .Where(e => e.Value.Errors.Count > 0)
+                       .SelectMany(x => x.Value.Errors)
+                       .Select(x => x.ErrorMessage).ToArray();
+
+                   var errorResponse = new ApiValidationErrorResponse
+                   {
+                       Errors = errors
+                   };
+
+                   return new BadRequestObjectResult(errorResponse);
+               };
+            });
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+        
+          
+           
+           
+
+
+
+           
+           
+
             return services;
         }
     }
--- API/Program.cs
+++ API/Program.cs
@@ -8,33 +8,27 @@ using Microsoft.EntityFrameworkCore;
 using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-// Add services to the container.
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-
-
-//AutoMapper
-builder.Services.AddAutoMapper(typeof(MappingProfiles));
-
-builder.Services.AddControllers();
-//Database connection
-builder.Services.AddDbContext<StoreContext>(x=>x.UseSqlite(connectionString));
 //Extensions
-builder.Services.AddApplicationServices();
-//Extensions/Swagger
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddControllers();
+  //Extensions/Swagger
 builder.Services.AddSwaggerDocumentation();
-//CORS
-builder.Services.AddCors(opt=>{
-    opt.AddPolicy("CorsPolicy", policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-});
-
 
 var app = builder.Build();
-
+// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseSwaggerDocumentaion();
+//Errorhandling
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+app.UseHttpsRedirection();
+app.UseRouting();
+//Middleware serve images from api
+app.UseStaticFiles();
+//Middleware cors
+app.UseCors("CorsPolicy");
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 //Migrations and DB Update
 using (var scope = app.Services.CreateScope())
 {
@@ -54,20 +48,5 @@ using (var scope = app.Services.CreateScope())
     }
     
 }
-
-// Configure the HTTP request pipeline.
-app.UseMiddleware<ExceptionMiddleware>();
-
-//Errorhandling
-app.UseStatusCodePagesWithReExecute("/errors/{0}");
-
-app.UseHttpsRedirection();
-//Middleware serve images from api
-app.UseStaticFiles();
-//Middleware cors
-app.UseCors("CorsPolicy");
-app.UseAuthorization();
-app.UseSwaggerDocumentaion();
-app.MapControllers();
 
 app.Run();
